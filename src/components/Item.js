8383595f0b219b3d0ea67ea10319793f82802aa2
Nick Landas <nick.landas@gmail.com>
2022-11-29 18:07:23 -0600

    commit tree 166a27d4b77a919abf7f021d51828e70abdfed90

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  
+  const[addItem, setAddItem] = useState("")
+  const inCart = addItem ? "in-cart" : ""
+
+  function handleAddToCart(item) { 
+    setAddItem((addItem) =>!addItem)
+  }
+  
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+  <button className="add" onClick={handleAddToCart}>{addItem ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import items from "../data/items";
 import Item from "./Item";
 
+
+
 function ShoppingList({ items }) {
+
+  const[selectedCategory, setSelectedCategory] = useState("All")
+  const handleFilteredSelection  = (event) =>{
+    //console.log(event.target.value)
+    setSelectedCategory(event.target.value)
+  }
+  const newSelectionArray = items.filter((item) => {
+    if (selectedCategory === "All"){
+      return true;
+    }else{
+      return item.category === selectedCategory;
+    }
+  })
+
+  const categorizedItems = newSelectionArray.map((item) => {
+    console.log(item)
+    
+
+  })
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilteredSelection}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -19,6 +42,7 @@ function ShoppingList({ items }) {
       </ul>
     </div>
   );
+       
 }
 
 export default ShoppingList;
